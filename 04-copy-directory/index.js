eb7cd9a94db41dbbd68824ececa818cdfde4a094
Nadezhda Budarina <hopechka@gmail.com>
2022-05-18 16:25:17 +0300

    refactor: make code prettier

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,11 +4,8 @@ const fs = require('fs');
 
 // 2. Создание папки files-copy в случае если она ещё не существует
 // { recursive: true } - помогает избежать ошибок, в случаях когда директория уже создана
-fs.promises.mkdir(path.join(__dirname, 'files-copy'), {
-  recursive: true,
-});
 const dest = path.join(__dirname, 'files-copy');
-console.log(dest);
+fs.promises.mkdir(dest, { recursive: true });
 
 // 3. Чтение содержимого папки files
 try {
