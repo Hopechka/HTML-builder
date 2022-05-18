b48703ed936378ca970616e1f3dff5fab3f19318
Nadezhda Budarina <hopechka@gmail.com>
2022-05-18 16:22:29 +0300

    feat: add resolve for task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+// 1. Импорт всех требуемых модулей
+const path = require('path');
+const fs = require('fs');
 
+// 2. Создание папки files-copy в случае если она ещё не существует
+// { recursive: true } - помогает избежать ошибок, в случаях когда директория уже создана
+fs.promises.mkdir(path.join(__dirname, 'files-copy'), {
+  recursive: true,
+});
+const dest = path.join(__dirname, 'files-copy');
+console.log(dest);
+
+// 3. Чтение содержимого папки files
+try {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      // 4. Копирование файлов из папки files в папку files-copy
+      fs.promises.copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(dest, file)
+      );
+    }
+  });
+} catch (err) {
+  console.log(err);
+}
